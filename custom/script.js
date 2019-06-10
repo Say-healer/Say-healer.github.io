ec6130151b816b6b1c4bcb9d8fa3fd4d6d59dc88
Healer <lnf0991@163.com>
2019-06-10 15:54:32 +0800

    Site updated: 2019-06-10 15:54:32

---
+++ custom/script.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.type = 'text/javascript';
+    hm.src = "https://hm.baidu.com/hm.js?b2c6e01d6e80f8edf7e5eeea291eaa53";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
